2ab6d499c0edc6afdddc69afced386fda55937e2
LocoServer <locoserver@locoserver-OEM.(none)>
2014-02-21 15:13:15 -0300

    Mudança do jogo para dois jogadores

---
+++ lib/dice_game/config.rb
@@ -0,0 +1,9 @@
+module DiceGame
+class Config
+
+	def initialize(dices, sides)
+		@dices = 2
+		@sides = 6
+	end
+end
+end
--- lib/dice_game/game.rb
+++ lib/dice_game/game.rb
@@ -2,21 +2,25 @@
 
 module DiceGame
 	class Game
+
 		def initialize
-		name = ask("What's your name?", "Invalid name") {|a| a.length >= 3 } 
-		p "Hi, #{name}, let's play!"
-		age = ask("How old are you?", "You must be 18") {|a| a.to_i >= 18 } 
-		@first_player = DiceGame::Player.new(name, age)
-		@second_player = DiceGame::Player.new("Robot", 42)
+		@first_player = create_player("FIRST")
+		@second_player = create_player("SECOND")
+		p "LET'S PLAY!!!"
 		@dice = DiceGame::Roller.new(2,6)
 		play
 		end
 
 		private
 
-		def winner
-			@first_player.score >= 5 || @second_player.score >= 5
-
+		def create_player(title)
+			p "-----------------------------------------------------------"
+			p "#{title} player"
+			name = ask("What's your name?", "Invalid name") {|a| a.length >= 3 } 
+			age = ask("How old are you?", "You must be 18") {|a| a.to_i >= 18 } 
+			p "OK, First Player is #{name}, #{age} years old. Good luck!"
+			p "-----------------------------------------------------------"
+			DiceGame::Player.new(name, age)
 		end
 
 		def ask(question, wrong_answer, &block)
@@ -30,18 +34,29 @@ module DiceGame
 		end
 
 		def play
+			@odd_even = ask("#{@first_player.name}, do you wanna be the Even or the Odd Player? Type 'even' or 'odd'", "Please, enter 'even' or 'odd'") {|a| a == "even" || a == "odd"}
+			odd_even
+			p "OK. #{@first_player.name}, you're the #{@odd_even} player, and #{@second_player.name}, you are the #{@other} player."
 			until winner
-				answer = ask("Do you wanna roll the dices or quit the game?", "Please, enter 'roll' or 'quit'") {|a| a == "roll" || a == "quit"}
+				answer = ask("Roll the dices or quit the game?", "Please, enter 'roll', 'quit' or just hit enter") {|a| a == "roll" || a == "quit" || a == ""}
 				if answer == "quit"
+					p "Hope to see you soon!"
 					exit
 				else
-					makeroll @first_player
-					makeroll @second_player										
+					makeroll								
 				end
 			end
-			p "PARABÉNS!! O GANHADOR É #{winner_player}"
+			p "PARABÉNS, #{winner_player}, VOCÊ GANHOU!!"
 		end
 
+		def odd_even
+			@other = (@odd_even == "even") ? "odd" : "even"
+		end
+
+		def winner
+			@first_player.score >= 5 || @second_player.score >= 5
+		end
+		
 		def winner_player
 			if @first_player.score >= 5
 				@first_player.name
@@ -50,14 +65,23 @@ module DiceGame
 			end
 		end
 
-		def makeroll(user)
+		def makeroll
 			score = @dice.roll
-			result = score.odd? ? -1 : 1
-			user.score += result
-			p "#{user.name} rolou: #{score} e o seu placar atual é #{user.score}"
+			if @odd_even == "odd" && score.odd?
+				@first_player.score += 1
+			elsif @odd_even == "odd" && score.even?
+				@second_player.score += 1
+			elsif @odd_even == "even" && score.even?
+				@first_player.score += 1
+			else 
+				@second_player.score += 1
+			end
+			p "-----------------------------------------------------------"
+			p "The number is #{score}"
+			p "#{@first_player.name}, your score is  #{@first_player.score}"
+			p "#{@second_player.name}, your score is #{@second_player.score}"
+			p "-----------------------------------------------------------"
 		end
- 
-		
 
 	end
 end
